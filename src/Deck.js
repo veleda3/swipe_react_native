c29f6826ba9ae9f0fa8ae29ce239179d8c1c10e1
Daniel Velez <velez.a.daniel@gmail.com>
2017-09-20 18:33:12 -0400

    changes to the table done

--- src/Deck.js
+++ src/Deck.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, PanResponder } from 'react-native';
 
 
 export default class Deck extends Component {
+  constructor(props) {
+    super(props);
+    // make sure to look at the docs for all the options for pnaResponders
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        console.log(gesture);
+      },
+      onPanResponderRelease: () => {}
+    });
+    this.state = { panResponder }
+  }
   renderCards() {
     return this.props.data.map(item => {
         return this.props.renderCard(item);
     })
-  }
-  
+  };
   render() {
     return (
-      <View>
+      <View {...this.state.panResponder.panHandlers}>
         {this.renderCards()}
       </View>
     );
